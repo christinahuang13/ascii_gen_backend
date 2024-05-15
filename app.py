bbd18844dcbef853f5e1ecd1662648e63b448299
Christina Huang <christinahuang13@gmail.com>
2024-05-15 11:34:10 -0700

    Update to use Dockerfile

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from src.text_noun_extractor import extract_nouns
 from pathlib import Path
 from src.text_to_ascii_background_generator import TextToAsciiGenerator
 from multiprocessing import Process
+import multiprocessing
 import os
 import logging
+import urllib
 
 supabase_client = create_supabase_clent()
 ascii_generator = TextToAsciiGenerator()
@@ -28,6 +30,7 @@ def async_synthesize_and_upload_image(*args):
     print(f"Successfully uploaded image for {noun} to {supabase_storage_path}!")
 
 def generate_ascii_images(request_id, text):
+    multiprocessing.set_start_method('spawn')
     most_freq, least_freq = extract_nouns(text)
     ascii_out_folder = f'/tmp/ascii_ims_{request_id}'
     Path(ascii_out_folder).mkdir(parents=True, exist_ok=True)
@@ -48,16 +51,32 @@ def create_ascii():
     # TODO: Validate data before adding to table
     table_result = supabase_client.table("ascii_submissions").insert(data).execute()
     response_id = table_result.data[0]['id']
-    print(data)
     generate_ascii_images(response_id, data['text'])
     response = jsonify(table_result.data)
     return response
+
+def convert_ascii_from_im_file(*args):
+    im_file = args[0]
+    out_file = ascii_generator.convert_ascii(im_file)
+    return out_file
+
+@flask_app.route('/test-ascii-generation', methods=['POST', 'OPTIONS'])
+def test_ascii_generation():
+    multiprocessing.set_start_method('spawn')
+    if request.method == 'OPTIONS':
+        return {}
+    local_im_file = '/app/src/images/v1_txt2img_0_53.png'
+    p = Process(target=convert_ascii_from_im_file, args=(local_im_file,))
+    p.start()
+    return "doing stuff for testing"
 
 if __name__ != '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.error')
     flask_app.logger.handlers = gunicorn_logger.handlers
     flask_app.logger.setLevel(gunicorn_logger.level)
+
 if __name__ == '__main__':
    port = int(os.getenv('PORT', 5000))
    flask_app.run(port=port, debug=True)
+
 
